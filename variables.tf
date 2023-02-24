eac8964e95cdbc8c6e788a3c247511ea81423c22
Vishnu4226 <52781872+Vishnu4226@users.noreply.github.com>
2023-02-24 21:57:25 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "ami"{
+ type = string
+}
+variable "instancetype"{
+ type = string
+}
+variable "keypair"{
+ type = string
+}
+variable "subnet"{
+ type = string
+}
+variable "vpc"{
+ type = string
+}
+variable "securitygroupid"{
+ type = string
+}
