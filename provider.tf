871a1b01d5b7d6c2c06e053e90ced0942a03ea66
Vishnu4226 <52781872+Vishnu4226@users.noreply.github.com>
2023-02-24 23:03:40 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAXVYQPHFK7HCELZ2J"
-  secret_key = "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"
+ # access_key = "AKIAXVYQPHFK7HCELZ2J"
+ # secret_key = "EjdXCPA95ftOJwRVKMIAs+7F9QTV3a6nDr8V1g"
 }
